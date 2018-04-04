b480d7bee2eedff09fbc39fef9a4620eef8e0611
Eimantas Šiugždinis <eimantas.siugzdinis@ktu.edu>
2018-04-04 15:50:15 +0300

    Revert "Full project"
    This is to later commit Laravel framework and my written code in different commits

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <h1>HOME</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam animi aspernatur dolorem dolorum ducimus esse harum itaque laboriosam libero nihil, nisi officia porro quae ratione recusandae sint totam voluptate!</p>
-    @endsection
-
-@section('sidebar')
-
-    @endsection
--- resources/views/inc/sidebar.blade.php
+++
@@ -1,5 +0,0 @@
-@section('sidebar')
-    <div class="well">
-        <h3>Sidebar</h3>
-        Sidebar test
-    </div>
